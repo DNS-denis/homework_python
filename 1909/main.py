2840339b8d6da4affaf2f8fc2c2af50ed8f209e6
DNS-denis <prostocel1990@gmail.com>
2022-10-20 13:15:53 +0300

    добавлено ДЗ

---
+++ 1909/main.py
@@ -0,0 +1,23 @@
+def dateValidate(day, month, year):
+    d31 = [1, 3, 5, 7, 8, 10, 12]
+    d30 = [4, 6, 9, 11]
+    if month == d31:
+        if 1 <= day <= 31:
+            return True
+        else:
+            return False
+    elif month == d30:
+        if 1 <= day <= 30:
+            return True
+        else:
+            return False
+    elif year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        if month == 2 and 1 <= day <= 29:
+            return True
+        else:
+            return False
+    elif year % 4 != 0 or year % 400 != 0:
+        if month == 2 and 1 <= day <= 28:
+            return True
+        else:
+            return False
---
+++ 1909/secondary.py
@@ -0,0 +1,3 @@
+from main import dateValidate
+
+print(dateValidate(20, 2, 1700))
