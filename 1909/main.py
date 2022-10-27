369ae47342ba0df126ef3c1b547fa06e8a5a5fb0
DNS-denis <113025409+DNS-denis@users.noreply.github.com>
2022-10-27 10:57:08 +0300

    Update main.py

--- 1909/main.py
+++ 1909/main.py
@@ -1,12 +1,12 @@
 def dateValidate(day, month, year):
     d31 = [1, 3, 5, 7, 8, 10, 12]
     d30 = [4, 6, 9, 11]
-    if month == d31:
+    if month in d31:
         if 1 <= day <= 31:
             return True
         else:
             return False
-    elif month == d30:
+    elif month in d30:
         if 1 <= day <= 30:
             return True
         else:
@@ -16,8 +16,7 @@ def dateValidate(day, month, year):
             return True
         else:
             return False
-    elif year % 4 != 0 or year % 400 != 0:
-        if month == 2 and 1 <= day <= 28:
-            return True
-        else:
-            return False
+    elif month == 2 and 1 <= day <= 28:
+        return True
+    else:
+        return False
